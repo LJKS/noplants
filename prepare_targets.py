75a5887464ca312101b87d8280e1b25c15cec59c
geronimocharlie <charlie.lange@posteo.de>
2020-03-02 17:11:28 +0100

    fixed bug in target preparaiton

--- prepare_targets.py
+++ prepare_targets.py
@@ -7,7 +7,7 @@ from PIL import Image
 THE_GOOD = [0,1,1] #cyan
 THE_BAD = [1,0,1] #magenta
 THE_UGLY = None #everything else
-subpics = 200
+subpics = 1
 crop_size = (256, 256, 3)
 origin_target_directory = 'stem_lbl_human'
 origin_data_directory = 'stem_data'
@@ -27,12 +27,15 @@ for img_name in listdir(origin_data_directory):
     img_data = img_data.astype(np.uint8)
     img_target = tf.convert_to_tensor(img_target)
     img_data = tf.convert_to_tensor(img_data)
+
     for i in range(subpics):
         save_path_target = 'stem_lbl_cropped_container/stem_lbl_cropped/' + img_name[0:-4] + '_crop_' + str(i) + '.png'
         save_path_data = 'stem_data_cropped_container/stem_data_cropped/' + img_name[0:-4] + '_crop_' + str(i) + '.png'
         rand_seed = np.random.rand()
-        sub_img_target = tf.image.random_crop(img_target, crop_size, rand_seed)
-        sub_img_data = tf.image.random_crop(img_data, crop_size, rand_seed)
+        tf.random.set_seed(rand_seed)
+        sub_img_target = tf.image.random_crop(img_target, crop_size)
+        tf.random.set_seed(rand_seed)
+        sub_img_data = tf.image.random_crop(img_data, crop_size)
         # convert back from tensor to np array
         sub_img_target = sub_img_target.numpy()
         sub_img_data = sub_img_data.numpy()
@@ -40,6 +43,10 @@ for img_name in listdir(origin_data_directory):
         save_img_target.save(save_path_target)
         save_img_data = Image.fromarray(sub_img_data).convert('RGB')
         save_img_data.save(save_path_data)
+        plt.imshow(sub_img_data)
+        plt.show()
+        plt.imshow(sub_img_target)
+        plt.show()
 
 """
 for img_str in listdir('lbl'):
