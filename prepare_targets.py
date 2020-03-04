c522458902999813dcfefb13678ebd8426ecf3e9
geronimocharlie <charlie.lange@posteo.de>
2020-03-04 16:43:39 +0100

    some more hyperparametrs added

--- prepare_targets.py
+++ prepare_targets.py
@@ -4,18 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from PIL import Image
+from hyperparametrs import
 THE_GOOD = [0,1,1] #cyan
 THE_BAD = [1,0,1] #magenta
 THE_UGLY = None #everything else
-subpics = 1
-crop_size = (256, 256, 3)
-origin_target_directory = 'stem_lbl_human'
-origin_data_directory = 'stem_data'
+
 assert listdir(origin_data_directory) == listdir(origin_target_directory)
 
-for img_name in listdir(origin_data_directory):
-    target_source_path = origin_target_directory + '/' + img_name
-    data_source_path = origin_data_directory + '/' + img_name
+for img_name in listdir(ORIGIN_DATA_DIRECTORY):
+    target_source_path = ORIGIN_LBL_DIRECTORY + '/' + img_name
+    data_source_path = ORIGIN_DATA_DIRECTORY + '/' + img_name
     img_target_human = image.imread(target_source_path)
     good = np.all(img_target_human == THE_GOOD, -1).astype(float)
     bad = np.all(img_target_human == THE_BAD, -1).astype(float)
@@ -28,14 +26,14 @@ for img_name in listdir(origin_data_directory):
     img_target = tf.convert_to_tensor(img_target)
     img_data = tf.convert_to_tensor(img_data)
 
-    for i in range(subpics):
-        save_path_target = 'stem_lbl_cropped_container/stem_lbl_cropped/' + img_name[0:-4] + '_crop_' + str(i) + '.png'
-        save_path_data = 'stem_data_cropped_container/stem_data_cropped/' + img_name[0:-4] + '_crop_' + str(i) + '.png'
+    for i in range(SUBPICS):
+        save_path_target = SAVE_LBL + img_name[0:-4] + '_crop_' + str(i) + '.png'
+        save_path_data = SAVE_DATA + img_name[0:-4] + '_crop_' + str(i) + '.png'
         rand_seed = np.random.rand()
         tf.random.set_seed(rand_seed)
-        sub_img_target = tf.image.random_crop(img_target, crop_size)
+        sub_img_target = tf.image.random_crop(img_target, CROP_SIZE)
         tf.random.set_seed(rand_seed)
-        sub_img_data = tf.image.random_crop(img_data, crop_size)
+        sub_img_data = tf.image.random_crop(img_data, CROP_SIZE)
         # convert back from tensor to np array
         sub_img_target = sub_img_target.numpy()
         sub_img_data = sub_img_data.numpy()
@@ -43,7 +41,7 @@ for img_name in listdir(origin_data_directory):
         save_img_target.save(save_path_target)
         save_img_data = Image.fromarray(sub_img_data).convert('RGB')
         save_img_data.save(save_path_data)
-        
+
 """
 for img_str in listdir('lbl'):
     img_str_old = 'lbl/'+img_str
