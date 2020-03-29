291aa074ebec8b2cf14b933d1a7001cfd122c1a9
geronimocharlie <charlie.lange@posteo.de>
2020-03-29 13:26:08 +0200

    update

--- hyperparametrs.py
+++ hyperparametrs.py
@@ -13,4 +13,13 @@ DATA_TEST = 'stem_data_cropped_container/stem_data_cropped'
 DATA_TEST_LBL = 'stem_lbl_cropped_container/stem_lbl_cropped'
 BATCH_SIZE_TEST = 1
 NUM_TESTS = 1
-MODEL_TEST_DIR = "models/ad_one_pic_only_flip/model_epoch0_step_7899"
+MODEL_TEST_DIR = "models/ad_one_pic_only_flip/model_epoch0_step_5899"
+
+# Data Preparation
+ORIGIN_LBL_DIRECTORY = 'stem_lbl_human'
+ORIGIN_DATA_DIRECTORY = 'stem_data'
+SUBPICS = 200
+CROP_SIZE = (256, 256, 3)
+# please create following directory
+SAVE_LBL = 'stem_lbl_cropped_container/stem_lbl_cropped/'
+SAVE_DATA = 'stem_data_cropped_container/stem_data_cropped/'
