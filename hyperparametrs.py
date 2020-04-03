94e00db73a2365e3fe5e593ca76248d9dcbb3a7c
LJKS <Leon-Schmid@gmx.de>
2020-04-03 19:20:51 +0200

    fixed broken model - bottlenecklayers were broken, are fixed now, network is proven to have basic functionality now

--- hyperparametrs.py
+++ hyperparametrs.py
@@ -1,12 +1,13 @@
 # HYPERPARAMETERS
 
 # Training
-MODEL_SAVE_DIR = 'models/ad_one_pic_only_flip'
+MODEL_SAVE_DIR = 'models/exp1'
 DATA_TRAIN_LBL = 'stem_lbl_cropped_container'
 DATA_TRAIN = 'stem_data_cropped_container'
 EPOCHS = 100
 SAVE_STEPS = 100
-
+BATCH_SIZE = 6
+CLOCK=False
 
 # Testing
 DATA_TEST = 'stem_data_cropped_container/stem_data_cropped'
